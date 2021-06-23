c40045e915453228d924993a76d23dbed89ec09a
SeungHak5161 <bsh93367@gmail.com>
2021-06-23 09:54:28 +0100

    erase vie

--- fire_detect_opencv/old_main(vid include).py
+++ fire_detect_opencv/old_main(vid include).py
@@ -39,9 +39,12 @@ def observe():
             fcm.sendMessage(str_now)
             # 탐지한 불꽃에 사각형으로 표시
             cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            fireStatus = True
             capture_time = savePhoto(now, frame)
             filename = str(capture_time.strftime('%Y-%m-%d %H:%M:%S')) + '.jpg'
             uploadPhoto(filename)
+            break
+        if fireStatus == True:
             break
 
 
@@ -49,6 +52,21 @@ def savePhoto(now, frame):
     cv.imwrite(picture_directory + str(now.strftime('%Y-%m-%d %H:%M:%S')) + ".jpg", frame)
     print('사진 저장 완료 (' + str(now.strftime('%Y-%m-%d %H:%M:%S')) + ".jpg)")
     return now
+
+
+def saveVideo():
+    print('녹화 시작')
+    global fireStatus
+    start_time = datetime.datetime.now()
+    while (capture.isOpened()):
+        now = datetime.datetime.now()
+        ret, frame = capture.read()
+        out.write(frame)
+        if start_time + datetime.timedelta(seconds=5) <= now:
+            fireStatus = False
+            print('녹화 종료')
+            print('영상 저장 완료 (' + capture_time.strftime('%Y-%m-%d %H:%M:%S') + ".avi)")
+            break
 
 
 def uploadPhoto(file):
@@ -62,10 +80,27 @@ def uploadPhoto(file):
     print("사진 업로드 완료")
 
 
+def uploadVideo(file):
+    blob = bucket.blob('detect_history/videos/' + file)
+    # new token, metadata 설정
+    new_token = uuid4()
+    metadata = {"firebaseStorageDownloadTokens": new_token}  # access token 필요
+    blob.metadata = metadata
+
+    blob.upload_from_filename(filename='/home/pi/2021_cap/fire_detect_opencv/detect_history/videos/' + file)
+    print("영상 업로드 완료")
+
+
 while True:
-    observe()
-        
+    if fireStatus == False:
+        observe()
+    else:
+        out = cv.VideoWriter(video_directory + capture_time.strftime('%Y-%m-%d %H:%M:%S') + '.avi', fourcc, 20.0,
+                             (640, 480))
+        saveVideo()
+        uploadVideo(capture_time.strftime('%Y-%m-%d %H:%M:%S') + '.avi')
 
 capture.release()
 out.release()
 cv.destroyAllWindows()
+
